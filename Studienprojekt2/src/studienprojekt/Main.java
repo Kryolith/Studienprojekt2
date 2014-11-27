a13a52d4b21e60ad0516eec4a3dfb27b55c267f0
Sebastian <Sebastian@134.147.168.209>
2014-11-27 17:50:07 +0100

    Imports aufgeräumt

--- Studienprojekt2/src/studienprojekt/Main.java
+++ Studienprojekt2/src/studienprojekt/Main.java
@@ -1,17 +1,10 @@
 
 package studienprojekt;
 
-import java.io.IOException;
-import javax.xml.parsers.ParserConfigurationException;
-import org.xml.sax.SAXException;
-import studienprojekt.osm.OSMCoordinate;
-import studienprojekt.osm.OSMMap;
-import studienprojekt.osm.OSMParser;
-
 public class Main {
     
     public static void main(String[] args) {
-        // dsjadskfdshj
+        
         // Construct and initialize the main object
         Mapper mapper = Mapper.getInstance();
         mapper.initialize();
