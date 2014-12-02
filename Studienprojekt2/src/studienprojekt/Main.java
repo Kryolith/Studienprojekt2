110a011933d697c38970dff8a86846efcbb90ef5
tatarmkq <martin.tatara@rub.de>
2014-12-02 17:22:46 +0100

    Merge origin/Martin into Martin
    Conflicts:
    	Studienprojekt2/src/studienprojekt/osm/OSMWay.java

--- Studienprojekt2/src/studienprojekt/Main.java
+++ Studienprojekt2/src/studienprojekt/Main.java
@@ -1,4 +1,3 @@
-
 package studienprojekt;
 
 import java.io.IOException;
@@ -9,6 +8,8 @@ import studienprojekt.osm.OSMMap;
 import studienprojekt.osm.OSMParser;
 
 public class Main {
+    
+//Dieses Kommentar gibt's nur im Martin-Branch!
     
     public static void main(String[] args) {
         
